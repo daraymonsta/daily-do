a111ef83c94d512867a7ba5b33173aabeeb7b264
daraymonsta <77724137+daraymonsta@users.noreply.github.com>
2022-02-16 12:48:53 +0000

    Create mid_letter.py

---
+++ mid_letter.py
@@ -0,0 +1,29 @@
+'''
+PURPOSE
+    function to return the middle letter
+    
+EXAMPLE
+    mid("abc") should return "b" and mid("aaaa") should return ""
+'''
+
+def mid(input_str):
+    # get string length
+    input_str_len = len(input_str)
+    
+    # if number is odd, then find middle letter
+    
+    # get middle letter position
+    middle_letter_pos = ((input_str_len-1) / 2)
+    
+    # check if the decimal part is zero
+    # (if true if it's even and has a middle letter)
+    if middle_letter_pos %1 == 0:
+        middle_letter = input_str[int(middle_letter_pos)]
+        print('Middle letter is {}'.format(middle_letter))
+        return middle_letter
+    else:
+        print('There is no middle letter')
+        return ""
+    
+input_str = "aBcDe"
+mid(input_str)
